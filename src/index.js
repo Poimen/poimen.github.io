54f73d797b0cc96e1005cf83363963d5807dbec1
Richard Shephard <richard.shephard@gmail.com>
2021-01-15 20:54:52 +0200

    Add hero section

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// TODO : add uglify
+
+(function() {
+  window.onscroll = function() {
+    const scrollLimit = 30;
+    const nav = document.getElementById('nav');
+
+    if(window.pageYOffset > scrollLimit) {
+      nav.classList.add('shadow-md');
+    } else {
+      nav.classList.remove('shadow-md');
+    }
+  };
+})();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
+    './src/**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        primary: '#011c55',
+        linkedin: '#0077B5'
+      },
+      backgroundColor: {
+        primary: '#011c55'
+      },
+      backgroundImage: theme => ({
+        'hero-img': 'url("../hero.jpg")'
+      })
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
